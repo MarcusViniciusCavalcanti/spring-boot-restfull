03fcaa5f2839292c8f0064c1397455bb60774537
Vinicius Cavalcanti <viniciuscavalcant@letsgrow.com.br>
2018-02-28 22:37:58 -0300

    init

---
+++ src/main/java/br/com/devdojo/exam_generator/ExamGenerator.java
@@ -0,0 +1,11 @@
+package br.com.devdojo.exam_generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamGenerator {
+	public static void main(String[] args) {
+		SpringApplication.run(ExamGenerator.class, args);
+	}
+}
